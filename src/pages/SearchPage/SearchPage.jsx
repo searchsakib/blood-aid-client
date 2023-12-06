1f955483005d4df9f74c15ac598bd633869c6ab1
searchsakib <searchsakib@gmail.com>
2023-12-06 18:40:01 +0600

    minor design change

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -24,69 +24,75 @@ const SearchPage = () => {
         <title>Blood Aid | Search Page</title>
       </Helmet>
       <h2 className="text-3xl py-10 font-semibold text-center">Search</h2>
-      <form onSubmit={handleSubmit} className="" action="">
-        <div>
-          <label htmlFor="" className="block p-3">
-            Blood Group
-          </label>
-          <select name="" id="" className="mx-3 px-3 outline outline-2">
-            <option value="">Select Blood Group</option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
-        </div>
+      <div className="flex gap-10  justify-center">
+        <form onSubmit={handleSubmit} className="" action="">
+          <div>
+            <label htmlFor="" className="block p-3">
+              Blood Group
+            </label>
+            <select name="" id="" className="mx-3 px-3 outline outline-2">
+              <option value="">Select Blood Group</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="" className="block p-3">
+              Districts
+            </label>
+            <select
+              onChange={(e) => setDistrict(e.target.value)}
+              name=""
+              id=""
+              className="mx-3 px-3 outline outline-2"
+            >
+              <option value="">Select District</option>
+              {Object.keys(towns)
+                .sort()
+                .map((town) => (
+                  <option key={town} value={town}>
+                    {' '}
+                    {town}{' '}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div>
+            <label htmlFor="" className="block p-3">
+              Upazilas
+            </label>
+            <select
+              name=""
+              id=""
+              className="mx-3 px-3 outline outline-2"
+              onChange={(e) => setUpazila(e.target.value)}
+            >
+              <option value="">Select Upazila</option>
+              {upazilas.map((singleUpazila) => {
+                return (
+                  <option key={singleUpazila} value={singleUpazila}>
+                    {singleUpazila}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <button className="btn mx-2 my-5  bg-[#05386B] text-white hover:text-[#05386B]  hover:bg-blue-50 hover:border-2 hover:border-[#05386B] rounded-none">
+            Search Donor
+          </button>
+        </form>
 
         <div>
-          <label htmlFor="" className="block p-3">
-            Districts
-          </label>
-          <select
-            onChange={(e) => setDistrict(e.target.value)}
-            name=""
-            id=""
-            className="mx-3 px-3 outline outline-2"
-          >
-            <option value="">Select District</option>
-            {Object.keys(towns)
-              .sort()
-              .map((town) => (
-                <option key={town} value={town}>
-                  {' '}
-                  {town}{' '}
-                </option>
-              ))}
-          </select>
+          <h2>This is where the magic happens</h2>
         </div>
-        <div>
-          <label htmlFor="" className="block p-3">
-            Upazilas
-          </label>
-          <select
-            name=""
-            id=""
-            className="mx-3 px-3 outline outline-2"
-            onChange={(e) => setUpazila(e.target.value)}
-          >
-            <option value="">Select Upazila</option>
-            {upazilas.map((singleUpazila) => {
-              return (
-                <option key={singleUpazila} value={singleUpazila}>
-                  {singleUpazila}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-        <button className="btn m-5  bg-[#05386B] text-white hover:text-[#05386B]  hover:bg-blue-50 hover:border-2 hover:border-[#05386B] rounded-none">
-          Search Donor
-        </button>
-      </form>
+      </div>
     </div>
   );
 };
