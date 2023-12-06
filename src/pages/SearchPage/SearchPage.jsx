5da540f95f55c41aec4837dd1240f44de2845d4b
searchsakib <searchsakib@gmail.com>
2023-12-06 17:50:38 +0600

    basic dropdown added

--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,13 +1,88 @@
 import { Helmet } from 'react-helmet-async';
+import towns from '../../data/towns';
+import { useEffect, useState } from 'react';
 
 const SearchPage = () => {
+  const [district, setDistrict] = useState();
+  const [upazila, setUpazila] = useState();
+  const [upazilas, setUpazilas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    if (district) {
+      setUpazilas(towns[district] || []);
+    }
+  }, [district]);
+
   return (
     <div className="max-w-screen-lg mx-auto min-h-[80vh]">
       <Helmet>
         <title>Blood Aid | Search Page</title>
       </Helmet>
       <h2 className="text-3xl py-10 font-semibold text-center">Search</h2>
-      <input type="text" />
+      <form onSubmit={handleSubmit} action="">
+        <div>
+          <label htmlFor="" className="block p-3">
+            Blood Group
+          </label>
+          <select name="" id="" className="mx-3 px-3 outline outline-2">
+            <option value="">Select Blood Group</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+          </select>
+        </div>
+
+        <div>
+          <label htmlFor="" className="block p-3">
+            Districts
+          </label>
+          <select
+            onChange={(e) => setDistrict(e.target.value)}
+            name=""
+            id=""
+            className="mx-3 px-3 outline outline-2"
+          >
+            <option value="">Select District</option>
+            {Object.keys(towns)
+              .sort()
+              .map((town) => (
+                <option key={town} value={town}>
+                  {' '}
+                  {town}{' '}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="" className="block p-3">
+            Upazilas
+          </label>
+          <select
+            name=""
+            id=""
+            className="mx-3 px-3 outline outline-2"
+            onChange={(e) => setUpazila(e.target.value)}
+          >
+            <option value="">Select Upazila</option>
+            {upazilas.map((singleUpazila) => {
+              return (
+                <option key={singleUpazila} value={singleUpazila}>
+                  {singleUpazila}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </form>
     </div>
   );
 };
